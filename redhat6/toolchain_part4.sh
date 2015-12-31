602904be3c0cc90deadc359fc319ef18813ed854
Felipe Noronha <felipenoris@gmail.com>
2015-12-31 09:42:02 -0300

    breaking parts

--- redhat6/toolchain_part4.sh
+++ redhat6/toolchain_part4.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# TODO incluir /usr/local/anaconda2/bin no PATH
 conda update conda
 
 # Ninja build system https://ninja-build.org/ (used to build llvm)
@@ -37,17 +38,5 @@ svn co http://llvm.org/svn/llvm-project/openmp/trunk openmp # Checkout Libomp (r
 cd ~/tmp
 mkdir llvm_build
 cd llvm_build
-#cmake -G Ninja ../llvm
-cmake ../llvm
-cmake --build .
-cmake --build . --target install
-#cmake -DCMAKE_INSTALL_PREFIX=/tmp/llvm -P cmake_install.cmake # para escolher o diretorio
-#cd ..
-#rm -rf llvm_build
-#rm -rf llvm
-
-# clang
-# http://llvm.org/releases/3.7.0/cfe-3.7.0.src.tar.xz
-#wget http://llvm.org/releases/3.7.0/cfe-3.7.0.src.tar.xz
-#tar xf cfe-3.7.0.src.tar.xz
-#cd cfe-3.7.0.src
+cmake -G Ninja ../llvm
+#cmake ../llvm
