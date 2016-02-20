56e8aff00e3ddc582e526e76d7e8d2729bb9c20a
Felipe Noronha <felipenoris@gmail.com>
2016-02-20 09:41:05 -0200

    fix new gcc libs location

--- redhat6/toolchain_part2.sh
+++ redhat6/toolchain_part2.sh
@@ -10,12 +10,31 @@ cd ..
 mkdir gcc_build
 cd gcc_build
 ../gcc_5_3_0_release/configure --prefix=/usr # default is /usr/local, see https://gcc.gnu.org/install/configure.html
-p="$(nproc --all)"
-make -j $p
+make -j "$(nproc --all)"
 make install
+
+#----------------------------------------------------------------------
+# Libraries have been installed in:
+#    /usr/lib/../lib64
+
+# If you ever happen to want to link against installed libraries
+# in a given directory, LIBDIR, you must either use libtool, and
+# specify the full pathname of the library, or use the `-LLIBDIR'
+# flag during linking and do at least one of the following:
+#    - add LIBDIR to the `LD_LIBRARY_PATH' environment variable
+#      during execution
+#    - add LIBDIR to the `LD_RUN_PATH' environment variable
+#      during linking
+#    - use the `-Wl,-rpath -Wl,LIBDIR' linker flag
+#    - have your system administrator add LIBDIR to `/etc/ld.so.conf'
+
+# See any operating system documentation about shared libraries for
+# more information, such as the ld(1) and ld.so(8) manual pages.
+#----------------------------------------------------------------------
+
 hash -r # forget about old gcc
 
 # Add new libraries to linker
-echo "/usr/local/lib64" > usrLocalLib64.conf
-mv usrLocalLib64.conf /etc/ld.so.conf.d/
+echo "/usr/lib64" > usrLib64.conf
+mv usrLib64.conf /etc/ld.so.conf.d/
 ldconfig
