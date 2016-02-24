91c2e10bfadfb70afc952b326ece3658a1331e4e
Felipe Noronha <felipenoris@gmail.com>
2016-02-24 09:30:58 -0300

    fix scripts path

--- redhat6/main.sh
+++ redhat6/main.sh
@@ -11,6 +11,7 @@
 # the default EC2 user is: ec2-user
 
 # yum -y install wget
+# cd
 # wget https://raw.githubusercontent.com/felipenoris/AWSFinance/master/redhat6/main.sh
 # chmod u+x main.sh
 
@@ -29,14 +30,14 @@ wget https://raw.githubusercontent.com/felipenoris/AWSFinance/master/redhat6/too
 wget https://raw.githubusercontent.com/felipenoris/AWSFinance/master/redhat6/toolchain_part10.sh
 wget https://raw.githubusercontent.com/felipenoris/AWSFinance/master/redhat6/final_check.sh
 
-source ./toolchain_part1.sh > ~/out_part1.txt 2>&1
-source ./toolchain_part2.sh > ~/out_part2.txt 2>&1
-source ./toolchain_part3.sh > ~/out_part3.txt 2>&1
-source ./toolchain_part4.sh > ~/out_part4.txt 2>&1
-source ./toolchain_part5.sh > ~/out_part5.txt 2>&1
-source ./toolchain_part6.sh > ~/out_part6.txt 2>&1
-source ./toolchain_part7.sh > ~/out_part7.txt 2>&1
-source ./toolchain_part8.sh > ~/out_part8.txt 2>&1
-source ./toolchain_part9.sh > ~/out_part9.txt 2>&1
-source ./toolchain_part10.sh > ~/out_part10.txt 2>&1
-source ./final_check.sh > ~/final_check.txt 2>&1
+source ~/toolchain_part1.sh > ~/out_part1.txt 2>&1
+source ~/toolchain_part2.sh > ~/out_part2.txt 2>&1
+source ~/toolchain_part3.sh > ~/out_part3.txt 2>&1
+source ~/toolchain_part4.sh > ~/out_part4.txt 2>&1
+source ~/toolchain_part5.sh > ~/out_part5.txt 2>&1
+source ~/toolchain_part6.sh > ~/out_part6.txt 2>&1
+source ~/toolchain_part7.sh > ~/out_part7.txt 2>&1
+source ~/toolchain_part8.sh > ~/out_part8.txt 2>&1
+source ~/toolchain_part9.sh > ~/out_part9.txt 2>&1
+source ~/toolchain_part10.sh > ~/out_part10.txt 2>&1
+source ~/final_check.sh > ~/final_check.txt 2>&1
