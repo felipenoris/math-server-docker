167452acfac0b85918cac3700a40928a7bfdf172
Felipe Noronha <felipenoris@gmail.com>
2017-08-08 07:34:46 -0300

    add GOROOT to child processes

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,7 +1,7 @@
 
 # Whitelist of environment variables for the subprocess to inherit
 # c.Spawner.env_keep = ['PATH', 'PYTHONPATH', 'CONDA_ROOT', 'CONDA_DEFAULT_ENV', 'VIRTUAL_ENV', 'LANG', 'LC_ALL']
-c.Spawner.env_keep = [ 'PATH', 'LD_LIBRARY_PATH', 'JAVA_HOME', 'CPATH', 'CMAKE_ROOT' ]
+c.Spawner.env_keep = [ 'PATH', 'LD_LIBRARY_PATH', 'JAVA_HOME', 'CPATH', 'CMAKE_ROOT', 'GOROOT' ]
 
 # set of usernames of admin users
 # 
